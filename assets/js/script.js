8a9c8b2eb3df7838c609d8c1f3c5868a924afa87
SiobhanP1 <siobhanphillips11@gmail.com>
2023-06-22 15:10:34 +0000

    Create js file and add api key, url and results modal constants

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+CONST API_KEY = "DqBCkyVjGHjCLUwT0bU-vqf3-Sc";
+CONST API_URL = "https://ci-jshint.herokuapp.com/api";
+CONST resultsModal = new bootstrap.Modal(document.getElementById("resultsmodal"));
